ed6d587b7f8cb591a4aedb98708176571cf14530
programmingprincess <jiaqiwu@asu.edu>
2018-11-03 18:45:59 -0500

    chrome extension storage

--- options.js
+++ options.js
@@ -3,43 +3,45 @@ function saveOptions(e) {
   var topSize = document.getElementById("topInput").value;
   document.getElementById("top-size").textContent = topSize;
 
+  var bottomSize = document.getElementById("bottomInput").value;
+  document.getElementById("bottom-size").textContent = bottomSize;
+
+  console.log(document.getElementById("store-list").value);
+
+  browser.storage.local.set({
+    store: document.querySelector("#store-list").value,
+    topSize: topSize
+  });
+
+  var storageitem = browser.storage.local.get('store');
+  storageitem.then((res) => {
+    console.log(`stored store is: ${res.store}`);
+  });
+
+  storageitem = browser.storage.local.get('topSize');
+  storageitem.then((res) => {
+    console.log(`stored topSize is: ${res.topSize}`);
+  });
+
   e.preventDefault();
 }
 
 function restoreOptions() {
   var stores = list_of_stores.stores;
   stores.sort();
-  console.log(stores);
   var options = "";
 
   for(var store in stores) {
-    options += "<option value=" + stores[store] + ">" + stores[store] + "</option>"
+    options += "<option value='" + stores[store] + "'>" + stores[store] + "</option>"
   }
 
   document.getElementById("store-list").innerHTML = options;
+
+  /*var storedStore = browser.storage.managed.get("selectedStore");
+  storedStore.then((res)=> {
+    document.
+  })*/
 }
-
-//from stackoverflow 
-//https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url/2499647#2499647
-function getJSONP(url, success) {
-
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0] 
-               || document.documentElement;
-
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
-    debugger
-
-}
-
-document.getElementById("top-size").textContent = 'lollll';
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
